9258863db1d3dffa82dc02b622f16ec571f0d7bb
temomikava <temsonmikava@gmail.com>
2022-02-20 17:51:50 +0400

    initiate commit

---
+++ GuessNumber/NumberGuessingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int myNumber = random.Next(1, 21);
+            int attempt = 5;
+            int yourNumber;
+            Console.WriteLine("Guess the number between 1 and 20");
+            while (attempt > 0)
+            {
+                bool parsedOrNot = int.TryParse(Console.ReadLine(), out yourNumber);
+                if (!parsedOrNot)
+                {
+                    Console.WriteLine("Please, enter a integer number");
+                }
+                else if (myNumber == yourNumber)
+                {
+                    Console.WriteLine("You Win!!!");
+                    break;
+                }
+                else if (myNumber > yourNumber)
+                {
+                    Console.WriteLine("My number is greather than that");
+                    attempt--;
+                }
+                else if (myNumber < yourNumber)
+                {
+                    Console.WriteLine("my number is less than that ");
+                    attempt--;
+                }
+            }
+            if (attempt == 0)
+                Console.WriteLine("You Lose");
+        }
+    }
+}
