5c14a6640174215ce92e1d7bd02a6570d30887ab
temomikava <temsonmikava@gmail.com>
2022-02-20 19:41:52 +0400

    add ability

--- GuessNumber/NumberGuessingGame/Program.cs
+++ GuessNumber/NumberGuessingGame/Program.cs
@@ -10,32 +10,50 @@ namespace NumberGuessingGame
             int myNumber = random.Next(1, 21);
             int attempt = 5;
             int yourNumber;
-            Console.WriteLine("Guess the number between 1 and 20");
-            while (attempt > 0)
+            string decision = "";         
+            do
             {
-                bool parsedOrNot = int.TryParse(Console.ReadLine(), out yourNumber);
-                if (!parsedOrNot)
+                Console.WriteLine("Guess the number between 1 and 20");
+                while (attempt > 0)
                 {
-                    Console.WriteLine("Please, enter a integer number");
+                    bool parsedOrNot = int.TryParse(Console.ReadLine(), out yourNumber);
+                    if (!parsedOrNot)
+                    {
+                        Console.WriteLine("Please, enter a integer number");
+                    }
+                    else if (myNumber == yourNumber)
+                    {
+                        Console.WriteLine("You Win!!!");
+                        break;
+                    }
+                    else if (myNumber > yourNumber)
+                    {
+                        Console.WriteLine("My number is greather than that");
+                        attempt--;
+                    }
+                    else if (myNumber < yourNumber)
+                    {
+                        Console.WriteLine("my number is less than that ");
+                        attempt--;
+                    }
                 }
-                else if (myNumber == yourNumber)
+                if (attempt == 0)
                 {
-                    Console.WriteLine("You Win!!!");
-                    break;
+                    Console.WriteLine("you lose");
                 }
-                else if (myNumber > yourNumber)
+                Console.WriteLine("do you want to play again? Yes/No");
+                decision = Console.ReadLine();
+                while (decision.ToLower() != "yes" && decision.ToLower() != "no")
                 {
-                    Console.WriteLine("My number is greather than that");
-                    attempt--;
+                    Console.WriteLine("please enter exactly \"yes\" or \"no\"");
+                    decision = Console.ReadLine();
                 }
-                else if (myNumber < yourNumber)
-                {
-                    Console.WriteLine("my number is less than that ");
-                    attempt--;
-                }
-            }
-            if (attempt == 0)
-                Console.WriteLine("You Lose");
+                if (decision.ToLower() == "yes")
+                    attempt = 5;
+            } while (decision.ToLower()!="no");
+
+            Console.WriteLine("thank you for playing");
+                         
         }
     }
 }
